08b057e8872cdfbb5a76b10540411f1c30f4d637
Petterson <firminopetterson@gmail.com>
2020-04-27 21:29:59 +0000

    all the basic functions added

--- 1st-version-without-backend/index.js
+++ 1st-version-without-backend/index.js
@@ -6,19 +6,38 @@ function handleSubmit(){
     let text = document.getElementById('input').value
     let date = document.getElementById('date').value
 
-    if(text !== ""){
+    if(text !== "" && date !== ""){
+
         toDo.date = date
         toDo.text = text
         addTask()
-    }else{
-        alert("The task field should not be blank.")
-    }
+
+        }else if(text !== "" && date === ""){
+
+                toDo.date = "There's no due date"
+                toDo.text = text
+                addTask()
+
+            }else{
+
+                alert("The task field should not be blank.")
+
+            }
     event.preventDefault()
 
 }
 function addTask(){
-    let taskbox = document.createElement('div')    
+    let taskbox = document.createElement('div') 
+    let textArea = document.createElement('p')
+    let dateArea = document.createElement('h2') 
+    let br = '<br>'
     taskbox.classList.add('task')
     taskbox.id = `${toDo.date}`
+    
+    textArea.innerHTML = `Task: ${br}${br}${toDo.text}`
+    dateArea.innerHTML = `Due to: ${br}${br}${toDo.date}`
+
     tasksContainer.appendChild(taskbox)
+    taskbox.appendChild(textArea)
+    taskbox.appendChild(dateArea)
 }
