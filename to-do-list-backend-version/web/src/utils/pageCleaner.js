a9514d4120844bf0c8eb8fe3027c4312a691395d
Petterson <58094725+hahuma@users.noreply.github.com>
2020-05-22 20:19:15 -0300

    Delete pageCleaner.js

--- to-do-list-backend-version/web/src/utils/pageCleaner.js
+++
@@ -1,8 +0,0 @@
-export default function pageCleaner(page = "login"){
-
-    const mainDiv = document.getElementById('mainDiv')
-    const style = document.querySelector('#additionalCss')
-    mainDiv.innerHTML = ''
-    style.setAttribute('href', `./pages/${page}/style.css`)
-
-}
